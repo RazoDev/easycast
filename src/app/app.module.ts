526cc915e2a18703d68b32218ff23bca4c083c6a
Carlos Razo <razodev@gmail.com>
2017-05-26 11:17:54 -0500

    easyCast version Desktop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { StreamComponent } from './components/stream/stream.component';
+import { SolucionesComponent } from './components/soluciones/soluciones.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    StreamComponent,
+    SolucionesComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
